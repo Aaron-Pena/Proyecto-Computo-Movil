a4f55a4aa934cda67d19c619f608d7f39a0c9824
Aaron Peña <aaronpena104@gmail.com>
2023-11-09 12:53:42 -0700

    Menu de registro y notas
    Visual de registro ya esta, notas no tiene nada mas que un label

--- app/src/main/java/com/compmovil/proyectomovil/Registro.java
+++ app/src/main/java/com/compmovil/proyectomovil/Registro.java
@@ -2,13 +2,33 @@ package com.compmovil.proyectomovil;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Registro extends AppCompatActivity {
+
+    private Button Btn_NewRegistrar;
+    private EditText confirmpasswordField,newpasswordField, newuserField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+
+        Btn_NewRegistrar = findViewById(R.id.Btn_NewRegistrar);
+        Btn_NewRegistrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Poner aqui que tome el texto. Comparar los passwordsField para ver que coincidan
+                //Que se agreguen a los usuario y no se repitan los usuarios
+                Toast.makeText(getApplicationContext(), "Agregando usuario...",Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(Registro.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
