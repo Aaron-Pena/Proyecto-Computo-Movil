771aa54bc500e82aa3351f117881597abf09f8c7
Aaron Peña <aaronpena104@gmail.com>
2023-11-10 18:06:15 -0700

    Login y registro
    login funciona con los datos de la lista y el registro puede agregar datos a esa lista.

--- app/src/main/java/com/compmovil/proyectomovil/MainActivity.java
+++ app/src/main/java/com/compmovil/proyectomovil/MainActivity.java
@@ -3,43 +3,70 @@ package com.compmovil.proyectomovil;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
-    private Button Btn_Entrar,Btn_Registrar;
+    private Button Btn_Entrar,Btn_Registrar,Btn_admin;
     private EditText passwordField, userField;
+
+    public static ArrayList<Users> userList = new ArrayList<>();
+    public static final String PREFS_NAME = "MyPrefsFile";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Recupera la lista de usuarios almacenada en preferencias compartidas
+        SharedPreferences preferences = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
+        Gson gson = new Gson();
+        String json = preferences.getString("userList", null);
+        Type type = new TypeToken<ArrayList<Users>>() {}.getType();
+        userList = gson.fromJson(json, type);
+
+        // Si la lista de usuarios es nula, inicialízala como una nueva lista
+        if (userList == null) {
+            userList = new ArrayList<>();
+            userList.add(new Users("Isaac", "123"));
+        }
+
         userField = findViewById(R.id.userField);
         passwordField = findViewById(R.id.passwordField);
 
         Btn_Entrar=findViewById(R.id.Btn_Entrar);
         Btn_Registrar = findViewById(R.id.Btn_Registrar);
+        Btn_admin = findViewById(R.id.Btn_admin);
+
 
 
         Btn_Entrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(passwordField.getText().toString().equals("")){
-                    Toast.makeText(getApplicationContext(), "Procesando...",Toast.LENGTH_SHORT).show();
-                    Intent intent = new Intent(MainActivity.this,Notas.class);
-                    startActivity(intent);
+                String userIngresado = userField.getText().toString();
+                String passwordIngresada = passwordField.getText().toString();
 
-                    intent.putExtra("_user",userField.getText().toString());
+                // Verifica las credenciales
+                if (verificarCredenciales(userIngresado, passwordIngresada)) {
+                    Toast.makeText(getApplicationContext(), "Procesando...", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(MainActivity.this, Notas.class);
+                    intent.putExtra("_user", userIngresado);
                     System.out.println("Mandara esto: "+userField.getText().toString());
                     startActivity(intent);
                     finish(); // Cierra la actividad actual
-
-                }else {
-                    Toast.makeText(getApplicationContext(), "Datos incorrectos. \nVuelva a intentar",Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Datos incorrectos. \nVuelva a intentar", Toast.LENGTH_SHORT).show();
                 }
             }
         });
@@ -52,5 +79,28 @@ public class MainActivity extends AppCompatActivity {
                     startActivity(intent);
             }
         });
+
+        Btn_admin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mostrarDatosUsuarios();
+            }
+        });
+
+    }
+
+    private boolean verificarCredenciales(String user, String password) {
+        for (Users u : userList) {
+            if (u.getUser().equals(user) && u.getPassword().equals(password)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void mostrarDatosUsuarios() {
+        for (Users user : MainActivity.userList) {
+            System.out.println("Usuario: " + user.getUser() + ", Contraseña: " + user.getPassword());
+        }
     }
 }
--- app/src/main/java/com/compmovil/proyectomovil/Registro.java
+++ app/src/main/java/com/compmovil/proyectomovil/Registro.java
@@ -3,32 +3,93 @@ package com.compmovil.proyectomovil;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.gson.Gson;
+
+import java.util.ArrayList;
+
 public class Registro extends AppCompatActivity {
 
     private Button Btn_NewRegistrar;
-    private EditText confirmpasswordField,newpasswordField, newuserField;
+    private EditText confirmpasswordField, newpasswordField, newuserField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
 
+        newuserField = findViewById(R.id.newuserField);
+        newpasswordField = findViewById(R.id.newpasswordField);
+        confirmpasswordField = findViewById(R.id.confirmpasswordField);
+
         Btn_NewRegistrar = findViewById(R.id.Btn_NewRegistrar);
         Btn_NewRegistrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //Poner aqui que tome el texto. Comparar los passwordsField para ver que coincidan
-                //Que se agreguen a los usuario y no se repitan los usuarios
-                Toast.makeText(getApplicationContext(), "Agregando usuario...",Toast.LENGTH_SHORT).show();
-                Intent intent = new Intent(Registro.this, MainActivity.class);
-                startActivity(intent);
+                String newUser = newuserField.getText().toString();
+                String newPassword = newpasswordField.getText().toString();
+                String confirmPassword = confirmpasswordField.getText().toString();
+
+                // Verifica que las contraseñas coincidan
+                if (newPassword.equals(confirmPassword)) {
+                    // Verifica que el nombre de usuario no exista
+                    if (!usuarioExistente(newUser)) {
+                        // Agrega el nuevo usuario a la lista en MainActivity
+                        agregarNuevoUsuario(newUser, newPassword);
+
+                        Toast.makeText(getApplicationContext(), "Usuario agregado correctamente", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(Registro.this, MainActivity.class);
+                        startActivity(intent);
+                    } else {
+                        Toast.makeText(getApplicationContext(), "El nombre de usuario ya existe", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(getApplicationContext(), "Las contraseñas no coinciden", Toast.LENGTH_SHORT).show();
+                }
             }
         });
+    }
+
+    private boolean usuarioExistente(String newUser) {
+        // Verifica si el nombre de usuario ya existe en la lista en MainActivity
+        for (Users u : MainActivity.userList) {
+            if (u.getUser().equals(newUser)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void agregarNuevoUsuario(String newUser, String newPassword) {
+        // Verifica que la lista de usuarios en MainActivity esté inicializada
+        if (MainActivity.userList == null) {
+            MainActivity.userList = new ArrayList<>();
+        }
+
+        // Verifica que el nombre de usuario no exista
+        if (!usuarioExistente(newUser)) {
+            // Agrega un nuevo usuario a la lista en MainActivity
+            MainActivity.userList.add(new Users(newUser, newPassword));
+
+            // Guarda la lista de usuarios en preferencias compartidas
+            SharedPreferences preferences = getSharedPreferences(MainActivity.PREFS_NAME, MODE_PRIVATE);
+            SharedPreferences.Editor editor = preferences.edit();
+            Gson gson = new Gson();
+            String json = gson.toJson(MainActivity.userList);
+            editor.putString("userList", json);
+            editor.apply();
+            System.out.println("User: "+newUser+"\nPassword: "+newPassword);
+            Toast.makeText(getApplicationContext(), "Usuario agregado correctamente", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(Registro.this, MainActivity.class);
+            startActivity(intent);
+        } else {
+            Toast.makeText(getApplicationContext(), "El nombre de usuario ya existe", Toast.LENGTH_SHORT).show();
+        }
     }
 }
